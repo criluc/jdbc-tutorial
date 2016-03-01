d7d136afc4aca7dfcc2445db50e6d23ba44bcdd7
Cristian Lucchesi <cristian.lucchesi@gmail.com>
2016-03-01 21:58:08 +0100

    Utilizzata la classe DBConfig nella creazione del db.

--- src/main/java/tutorial/jdbc/DBCreator.java
+++ src/main/java/tutorial/jdbc/DBCreator.java
@@ -4,7 +4,6 @@
 package tutorial.jdbc;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -20,15 +19,11 @@ public class DBCreator {
           + "name VARCHAR(255), surname VARCHAR(255))";
 
   public static void main(String[] args) throws SQLException {
-    System.out.print("Eseguo la connessione al db...");
-
-    try (Connection con = DriverManager.getConnection(
-        DBConfig.DB_URL, DBConfig.DB_USER, DBConfig.DB_PASSWORD)) {
+    try (Connection con = DBConfig.getConnection()) {
 
       try (Statement stmt = con.createStatement()) {
         //Esegue la query
         stmt.executeUpdate(CREATE_DB_DDL);
-        System.out.print("Tabelle create.");
       }
     }
   }
